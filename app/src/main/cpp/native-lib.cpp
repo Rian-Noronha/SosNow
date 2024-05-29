ced32bb08e91daf5fe6233d4e6bed9fa64070505
Rian-Noronha <riannoronha011@gmail.com>
2024-05-29 18:59:44 -0300

    Remover native-lib.cpp e CMakeLists.txt do repositório Git

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_rn_sosnow_map_Keys_apiKey(JNIEnv *env, jobject thiz) {
-    return env ->NewStringUTF("AIzaSyDQC7IjbyoaHX0mhDpLA6o2j-Q6FyB_U_w");  /* Here Api key*/
-}
